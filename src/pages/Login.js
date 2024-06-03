61527bf4d8866a9b6db0ffc74df636b91b04b2bd
hajhosseinico <hajhosseinico@gmail.com>
2024-06-03 14:49:54 -0400

    A

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -25,39 +25,39 @@ const Login = () => {
 
   return (
     <div className="background-wrapper">
-      <div className="login-container">
-        <div className="login-header">
-          <img src={logo} alt="Fitclub" className="logo" />
-          <p>یک اشتراک برای تمامی کلاس ها</p>
+        <div className="login-container">
+            <img src={logo} alt="Fitclub" className="logo" />
+            <img src={image1} alt="Class 1" className="full-width-image" />
+            <div className="login-header">
+                <p>یک اشتراک برای تمامی کلاس ها</p>
+            </div>
+            <form className="login-form" onSubmit={handleLogin}>
+                <label htmlFor="phone" className="right-aligned">شماره موبایل</label>
+                <input
+                    type="text"
+                    id="phone"
+                    name="phone"
+                    placeholder="091********"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                />
+
+                <label htmlFor="password" className="right-aligned">رمز عبور</label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="********"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                {error && <p className="error">{error}</p>}
+                <button type="submit" className="login-button">ورود</button>
+            </form>
         </div>
-        <div className="login-cards">
-          <img src={image1} alt="Class 2" className="card-image" />
-        </div>
-        <form className="login-form" onSubmit={handleSubmit}>
-          <label htmlFor="phone" className="right-aligned">شماره موبایل</label>
-          <input
-            type="text"
-            id="phone"
-            name="phone"
-            placeholder="091********"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-          />
-          <label htmlFor="password" className="right-aligned">رمز عبور</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="********"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button type="submit" className="login-button">ورود</button>
-        </form>
-        {message && <p>{message}</p>}
-      </div>
     </div>
-  );
+);
 };
 
 export default Login;
