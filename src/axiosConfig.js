748a0a13a9caf64c10cae8168caf41b8b778a4ef
hajhosseinico <hajhosseinico@gmail.com>
2024-06-03 20:54:18 -0400

    A

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://18.118.119.144/:3000',
+  baseURL: 'http://18.118.119.144:3000', // Change this to your backend URL
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
 export default api;
