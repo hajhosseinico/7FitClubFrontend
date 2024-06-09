121a61292c80f0afa5a2ff0859fd6f081bcc1ae1
hajhosseinico <hajhosseinico@gmail.com>
2024-06-09 10:40:55 -0400

    A

---
+++ build/precache-manifest.fd7bee0526287a2c8d8ff5db7eb95cc0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65e5c14923755bfdc641eb00b3cf44f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b27df4698a32b87861ea",
+    "url": "/static/css/main.73fa6f9c.chunk.css"
+  },
+  {
+    "revision": "d2bfa501e7ef3fa11c81",
+    "url": "/static/js/2.dc256586.chunk.js"
+  },
+  {
+    "revision": "da3d3b6d084d6d8a7821",
+    "url": "/static/js/3.294bdbb1.chunk.js"
+  },
+  {
+    "revision": "b27df4698a32b87861ea",
+    "url": "/static/js/main.9c802486.chunk.js"
+  },
+  {
+    "revision": "e7e76a5d24448cbc8075",
+    "url": "/static/js/runtime~main.3c24b8e7.js"
+  },
+  {
+    "revision": "8094817f971e1bd92c64ce8f0314c89c",
+    "url": "/static/media/background.8094817f.png"
+  },
+  {
+    "revision": "5d6cd32d1f7b2aa1da75b3752a8cafe7",
+    "url": "/static/media/cards.5d6cd32d.png"
+  },
+  {
+    "revision": "e8ba0555909a0a83e63787860c63fa24",
+    "url": "/static/media/logo.e8ba0555.png"
+  },
+  {
+    "revision": "b8fdbd1cd84293c0d09563b7d70d4826",
+    "url": "/static/media/texgyreadventor-bold.b8fdbd1c.otf"
+  }
+]);
