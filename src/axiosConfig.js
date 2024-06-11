fa2c02398c34676726af924ef397104add8ee06c
hajhosseinico <hajhosseinico@gmail.com>
2024-06-11 18:01:59 -0400

    A

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://3.133.158.10:3000', // Update this to your new backend URL
+  baseURL: process.env.REACT_APP_API_URL, // Use the environment variable
   headers: {
     'Content-Type': 'application/json'
   }
