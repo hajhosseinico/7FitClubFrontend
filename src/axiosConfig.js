0e2cd0e1fff6064dba2c857dfcf99d9538eb09ea
hajhosseinico <hajhosseinico@gmail.com>
2024-06-11 18:28:08 -0400

    A

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const baseURL = window.location.hostname === '7fitclub.com' || window.location.hostname === 'www.7fitclub.com' 
+  ? process.env.REACT_APP_API_URL 
+  : process.env.REACT_APP_API_URL_IP;
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // Use the environment variable
+  baseURL: baseURL, // Use the correct base URL based on the hostname
   headers: {
     'Content-Type': 'application/json'
   }
